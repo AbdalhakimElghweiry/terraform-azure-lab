e7134bc56988464e9e811c0cdb01e7b65bcb2e04
AbdalhakimElghweiry <kimboelgwere@gmail.com>
2026-06-12 22:21:10 +0200

    feat: Configure remote state backend in Azure Storage

--- providers.tf
+++ providers.tf
@@ -1,14 +1,22 @@
 # providers.tf
-# Configures the Azure provider for Terraform
 terraform {
-    required_version = ">= 1.0"
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 3.0"
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
-        }
-            }
+  }
+
+  # REMOTE STATE BACKEND
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstate26565"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
-    features {}
+  features {}
 }
